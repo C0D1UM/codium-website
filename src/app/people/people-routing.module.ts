7aff66c421e54c4b52e31fc04a9b7f32a21134a2
jiro412j <jiro412j@gmail.com>
2018-05-08 00:17:30 +0700

    Upgrade to angular 6 and add people list page

--- src/app/people/people-routing.module.ts
+++ src/app/people/people-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PeopleListComponent } from './people-list/people-list.component';
 
-export const peopleRoutes: Routes = [];
+export const peopleRoutes: Routes = [
+  {
+    path: 'list',
+    component: PeopleListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(peopleRoutes)],
