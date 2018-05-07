15df460f104aeced2d1b41e3d967d76b8f507a72
jiro412j <jiro412j@gmail.com>
2018-05-08 01:34:46 +0700

    Remove commented code

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -21,22 +21,6 @@ export class NavbarComponent implements OnInit {
       this.checkInverseNavbar();
       $('#navbarSupportedContent').removeClass('collapsed').removeClass('show');
     });
-    // $(document).ready(() => {
-    //   $(window).scroll(function () {
-    //     const scroll = $(window).scrollTop();
-    //     if (scroll > 200) {
-    //       $('.navbar').css('background', 'rgba(21, 39, 61, 0.9)');
-    //       $('nav').css('padding-top', '10px');
-    //       $('#logoWhite').css('display', 'block');
-    //       $('#logoColor').css('display', 'none');
-    //     } else {
-    //       $('.navbar').css('background', 'transparent');
-    //       $('nav').css('padding-top', '40px');
-    //       $('#logoWhite').css('display', 'none');
-    //       $('#logoColor').css('display', 'block');
-    //     }
-    //   });
-    // });
   }
 
   checkInverseNavbar() {
