5119835ea52e1fda15e8b6432996312b4a6fb2a1
Jiroj Kaewmanee <jiro412j@gmail.com>
2018-05-08 20:00:10 +0700

    First people page

--- src/app/people/people-routing.module.ts
+++ src/app/people/people-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PeopleListComponent } from './people-list/people-list.component';
+import { PacComponent } from './people-detail/pac/pac.component';
 
 export const peopleRoutes: Routes = [
   {
     path: 'list',
     component: PeopleListComponent
+  },
+  {
+    path: 'pac',
+    component: PacComponent
   }
 ];
 
