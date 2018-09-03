a96e29796bec7a7eaea7f89e13c8b6926d894acf
Jiroj Kaewmanee <>
2018-09-03 19:21:08 +0700

    More people detail

---
+++ src/app/people/people-detail/nook/nook.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nook',
+  templateUrl: './nook.component.html',
+  styleUrls: ['./nook.component.scss']
+})
+export class NookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/people/people.module.ts
+++ src/app/people/people.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { PeopleRoutingModule } from './people-routing.module';
 import { PeopleListComponent } from './people-list/people-list.component';
 import { PacComponent } from './people-detail/pac/pac.component';
+import { BoomComponent } from './people-detail/boom/boom.component';
+import { NookComponent } from './people-detail/nook/nook.component';
+import { SuayComponent } from './people-detail/suay/suay.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PeopleRoutingModule
   ],
-  declarations: [PeopleListComponent, PacComponent]
+  declarations: [PeopleListComponent, PacComponent, BoomComponent, NookComponent, SuayComponent]
 })
 export class PeopleModule { }
